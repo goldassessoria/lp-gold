f99f5672891076d6ae930cff0a5922d6da7ce1e8
Gold Assessoria Marketing Para Pizzarias <info.goldassessoria@gmail.com>
2025-09-18 02:50:54 +0000

    recebi o seguinte email:
    ear Customer,
    
    We have detected a publicly acc

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -4,14 +4,14 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhnXgGiAWMQHPR1bAvAqO_eg0dqJvTdHs",
-  authDomain: "gold-assessoria-2.firebaseapp.com",
-  databaseURL: "https://gold-assessoria-2-default-rtdb.firebaseio.com",
-  projectId: "gold-assessoria-2",
-  storageBucket: "gold-assessoria-2.firebasestorage.app",
-  messagingSenderId: "420374479890",
-  appId: "1:420374479890:web:63902e2f4d33b4f99804cb",
-  measurementId: "G-70ZFR61DQD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
